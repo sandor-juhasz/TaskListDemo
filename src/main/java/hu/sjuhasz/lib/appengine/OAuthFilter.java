10d0a00a9a713e7ff5246755d372059b384c8462
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2014-10-13 18:41:59 +0200

    Adding configurable OAuth2 filter.

--- src/main/java/hu/sjuhasz/lib/appengine/OAuthFilter.java
+++ src/main/java/hu/sjuhasz/lib/appengine/OAuthFilter.java
@@ -4,114 +4,51 @@ import hu.sjuhasz.demo.appengine.todolistdemo.Utils;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
-import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
-import com.google.api.client.auth.oauth2.AuthorizationCodeResponseUrl;
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.auth.oauth2.TokenResponse;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserServiceFactory;
 
-public class OAuthFilter implements Filter {
+public class OAuthFilter extends AbstractAppEngineOAuthFilter {
 
 	private AuthorizationCodeFlow flow;
-	
-	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
-		
-	}
 
-	/**
-	 * TODO: get auth code path from filter config.
-	 * Make the Flow available as ThreadLocal.
+	/*
+	 * Flow-related methods
 	 */
-	public void doFilter(
-			final ServletRequest request, 
-			final ServletResponse response,
-			final FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest)request;
-		HttpServletResponse resp = (HttpServletResponse)response;
-		
-		System.out.println("[OAuthFilter] processing "+req.getRequestURI());		
-		if (req.getPathInfo() != null && req.getPathInfo().equals("/authCode")) {
-			System.out.println("[OAuthFilter] authCode path was detected.");
-			
-		    StringBuffer buf = req.getRequestURL();
-		    if (req.getQueryString() != null) {
-		      buf.append('?').append(req.getQueryString());
-		    }
-		    AuthorizationCodeResponseUrl responseUrl = new AuthorizationCodeResponseUrl(buf.toString());
-		    String code = responseUrl.getCode();
-		    if (responseUrl.getError() != null) {
-		      resp.getWriter().println(responseUrl.getError());
-		    } else if (code == null) {
-		      resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-		      resp.getWriter().print("Missing authorization code");
-		    } else {
-		        if (flow == null) {
-		          flow = Utils.newFlow();
-		        }
-		        String redirectUri = "http://localhost:8888/authCode";
-		        TokenResponse tokenResponse = flow.newTokenRequest(code).setRedirectUri(redirectUri).execute();
-		        User user = UserServiceFactory.getUserService().getCurrentUser();
-		        if (user == null) {
-		        	resp.getWriter().println("Cannot identify user while processing authorization code.");
-		        } else {
-		        	String userId = user.getUserId();
-			        Credential credential = flow.createAndStoreCredential(tokenResponse, userId);
-			        resp.sendRedirect("/");
-		        }
-		    }						
-		} else {
-			User user = UserServiceFactory.getUserService().getCurrentUser();
-			if (user != null) {
-				System.out.println("[OAuthFilter] User "+user.getUserId()+" was detected with email "+user.getEmail()+".");
-				if (flow == null)				
-					flow = Utils.newFlow();
-				Credential credential = flow.loadCredential(user.getUserId());
-				if (credential == null || credential.getAccessToken() == null) {
-					System.out.println("[OAuthFilter] no credential was found or access token is null. Reauthorizing.");
-				    AuthorizationCodeRequestUrl authorizationUrl = flow.newAuthorizationUrl();
-				    authorizationUrl.setRedirectUri("http://localhost:8888/authCode");
-				    String location = authorizationUrl.build();
-				    System.out.println("[OAuthFilter] Redirecting to "+location);
-				    resp.sendRedirect(location);
-				} else {
-					System.out.println("[OAuthFilter] existing credential for user was found.");
-					System.out.println("[OAuthFilterFilter] calling protected resource. "+req.getRequestURI());
-					try {
-						chain.doFilter(request, response);
-					} catch (Exception e) {
-						if (credential.getAccessToken() == null) {
-							System.out.println("[OAuthFilter] null access token was detected after processing protected resource. Reauthorizing...");
-						    AuthorizationCodeRequestUrl authorizationUrl = flow.newAuthorizationUrl();
-						    authorizationUrl.setRedirectUri("http://localhost:8888/");
-						    String location = authorizationUrl.build();
-						    System.out.println("[OAuthFilter] Redirecting to "+location);
-						    resp.sendRedirect(location);							
-						}
-					}
-				}
-			} else {
-				if (req.getRequestURI().startsWith("/_ah/")) {
-					System.out.println("[OAuthFilter] Processing admin request.");
-					chain.doFilter(request, response);
-				}
-				System.out.println("[OAuthFilter] User is not logged in. Please configure user login first.");
-			}
-		} 
+	@Override
+	protected AuthorizationCodeFlow getFlow() throws IOException {
+		if (flow == null) {
+			flow = Utils.newFlow();
+		}
+		return flow;
 	}
 
-	public void destroy() {
+	/*
+	 * User info methods
+	 */
+	
+
+	/*
+	 * Authorization callback-related methods
+	 */
+	
+	@Override
+	protected boolean isAuthorizationCodeCallbackRequest(HttpServletRequest req) {
+		return req.getPathInfo() != null && req.getPathInfo().equals("/authCode");
 	}
 
+	@Override
+	protected String getAuthorizationCodeCallbackUri(HttpServletRequest req) {
+		return "http://localhost:8888/authCode";
+	}
+
+	@Override
+	protected void onSuccessfulAuthorization(HttpServletRequest req,
+			HttpServletResponse resp) throws IOException {
+		resp.sendRedirect("/");
+	}
+	
 }
